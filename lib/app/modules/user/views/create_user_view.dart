23e01361cc8d9c05f2d7201635b09550aceef728
Claudinei <dinei_aparecido@hotmail.com>
2024-02-15 23:30:59 -0300

    update user screen

--- lib/app/modules/user/views/create_user_view.dart
+++ lib/app/modules/user/views/create_user_view.dart
@@ -8,93 +8,88 @@ class CreateUserView extends GetView<UserController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro de Usuário'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 12, right: 12, bottom: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                const SizedBox(
-                  child: Text(
-                    'Informações do Usuário',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontFamily: 'Poppins',
-                      fontSize: 20,
-                    ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12, right: 12, bottom: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const SizedBox(
+                child: Text(
+                  'Informações do Usuário',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
                   ),
                 ),
-                Divider(
-                  height: 5,
-                  thickness: 3,
-                  color: Colors.orange.shade500,
-                ),
-              ],
-            ),
+              ),
+              Divider(
+                height: 5,
+                thickness: 3,
+                color: Colors.orange.shade500,
+              ),
+            ],
           ),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.only(left: 12, right: 12, top: 5),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      suffixIcon: Icon(Icons.person),
-                      labelText: 'Nome Completo',
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      suffixIcon: Icon(Icons.person),
-                      labelText: 'Login',
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      suffixIcon: Icon(Icons.lock),
-                      labelText: 'Senha',
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'CANCELAR',
-                          style: CustomTextStyle.button2(context),
-                        )),
-                    ElevatedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'SALVAR',
-                          style: CustomTextStyle.button(context),
-                        )),
-                  ],
-                )
-              ],
-            ),
-          ))
-        ],
-      ),
+        ),
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.only(left: 12, right: 12, top: 5),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.person),
+                    labelText: 'Nome Completo',
+                    border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.person),
+                    labelText: 'Login',
+                    border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.lock),
+                    labelText: 'Senha',
+                    border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'CANCELAR',
+                        style: CustomTextStyle.button2(context),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'SALVAR',
+                        style: CustomTextStyle.button(context),
+                      )),
+                ],
+              )
+            ],
+          ),
+        ))
+      ],
     );
   }
 }
