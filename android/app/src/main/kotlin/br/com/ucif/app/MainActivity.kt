a0a39ff296d8203095010771f2209b29b6e481d9
Claudinei Fernandes <dinei_aparecido@hotmail.com>
2026-02-09 10:25:09 -0300

    updates

---
+++ android/app/src/main/kotlin/br/com/ucif/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.ucif.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/br/com/ucif/ucif/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package br.com.ucif.ucif
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
